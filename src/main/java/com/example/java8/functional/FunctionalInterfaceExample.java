b61cf0c13df09592a2a42c2e57abb99b2104015e
Anjalee Badugodahewa <anjaleesakunthala@gmail.com>
2023-08-21 21:01:55 +0530

    Functional Interface Example

--- src/main/java/com/example/java8/functional/FunctionalInterfaceExample.java
+++ src/main/java/com/example/java8/functional/FunctionalInterfaceExample.java
@@ -3,4 +3,12 @@ package com.example.java8.functional;
 @FunctionalInterface
 public interface FunctionalInterfaceExample {
     void print(String msg);
+
+    default void method1(){
+        System.out.println("Default method");
+    }
+
+    static void method2(){
+        System.out.println("Static method");
+    }
 }
